687c9df8afd8f5849ffd967da14b97a879de37c7
Andrey Ilnitsky <andrey.ilnitsky@gmail.com>
2022-07-06 16:40:11 -0700

    + TlsHandler.state as volatile

--- src/DotNetty.Handlers/Tls/TlsHandler.cs
+++ src/DotNetty.Handlers/Tls/TlsHandler.cs
@@ -33,7 +33,7 @@ namespace DotNetty.Handlers.Tls
         readonly MediationStream mediationStream;
         readonly TaskCompletionSource closeFuture;
 
-        TlsHandlerState state;
+        volatile TlsHandlerState state;
         int packetLength;
         volatile IChannelHandlerContext capturedContext;
         BatchingPendingWriteQueue pendingUnencryptedWrites;
